0e00a3ce8f3f04834f32eab05c25a967acfb31a6
Omeshwari Jakkula <jakko01@pfw.edu>
2023-03-09 16:16:08 -0500

    adds additional files.

---
+++ test/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+// eslint-disable-next-line
+import { Login } from './login';
+// // eslint-disable-next-lines
+import { register } from "./register";
+
+
+function App() {
+  return (
+  
+      <>
+      <Login/>
+      </>
+    
+  );
+    
+
+}
+
+
+export default App;
