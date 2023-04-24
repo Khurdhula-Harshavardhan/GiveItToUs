b80ac2d007ce286aa7ef8171649a4df893e94abd
Harsha <khurdhulaharshavardhan@gmail.com>
2023-04-24 13:14:44 -0400

    installs crypt package to store user passwords which should be hashed.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,81 @@
 import "./Register.css"
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react"
+import { useEffect, useState } from "react";
+import bcrypt from "bcryptjs";
 
 function Register(){
 
 const navigate = useNavigate();
+
+const [formData, setFormData] = useState({
+  firstName: "",
+  lastName: "",
+  emailAddress: "",
+  phoneNumber: "",
+  addLineOne: "",
+  dob: "",
+  gender: "Male",
+  username: "",
+  password: "",
+  confirmPassword: "",
+});
+
+//every change within the form data, must reflect in our state.
+const storeUserData = async () => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(userData.password, salt);
+  const dataToStore = { ...userData, password: hashedPassword };
+  localStorage.setItem("userData", JSON.stringify(dataToStore));
+};
+
+const checkEmptyFields = (page) => {
+    const requiredFields = {
+      1: ["firstName", "lastName"],
+      2: ["emailAddress", "phoneNumber"],
+      3: ["addLineOne"],
+      4: ["dob", "gender", "username", "password", "confirmPassword"],
+    };
+
+    for (const field of requiredFields[page]) {
+      if (formData[field] === "") {
+        alert(`Please fill the ${field} field.`);
+        return false;
+      }
+    }
+    return true;
+  };
+
+
+
+//lets validate the form data at the end of submission.
+const validateForm = () => {
+  for (const key in formData) {
+    if (formData[key] === "" && key !== "addLineTwo") {
+      alert(`Please fill the ${key} field.`);
+      return false;
+    }
+  }
+  if (formData.password !== formData.confirmPassword) {
+    alert("Passwords do not match.");
+    return false;
+  }
+  return true;
+};
+
+//when user hits submit, based on the fact if user already exists we must then register them.
+const handleFormSubmit = async (e) => {
+  e.preventDefault();
+  if (validateForm()) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(formData.password, salt);
+    const storedData = { ...formData, password: hashedPassword };
+    localStorage.setItem("userData", JSON.stringify(storedData));
+    setTimeout(function () {
+      alert("Your Form Successfully Signed up");
+      navigate("/", { replace: true });
+    }, 800);
+  }
+};
 
 useEffect(() => {
 const slidePage = document.querySelector(".slide-page");
@@ -148,87 +219,89 @@ return(
       </div>
     </div>
     <div className="form-outer">
-      <form action="#">
-        <div className="page slide-page">
-          <div className="title">Basic Info:</div>
-          <div className="field">
-            <div className="label">First Name</div>
-            <input id="firstName" type="text" required/>
+        <form action="#">
+          <div className="page slide-page">
+            <div className="title">Basic Info:</div>
+            <div className="field">
+              <div className="label">First Name</div>
+              <input id="firstName" type="text" required />
+            </div>
+            <div className="field">
+              <div className="label">Last Name</div>
+              <input id="lastName" type="text" required />
+            </div>
+            <div className="field">
+              <button className="firstNext next">Next</button>
+            </div>
           </div>
-          <div className="field">
-            <div className="label">Last Name</div>
-            <input id="lastName" type="text" required/>
+          <div className="page">
+            <div className="title">Contact Info:</div>
+            <div className="field">
+              <div className="label">Email Address</div>
+              <input id="emailAddress" type="email" required />
+            </div>
+            <div className="field">
+              <div className="label">Phone Number</div>
+              <input id="phoneNumber" type="Number" required />
+            </div>
+            <div className="field btns">
+              <button className="prev-1 prev">Previous</button>
+              <button className="next-1 next">Next</button>
+            </div>
           </div>
-          <div className="field">
-            <button className="firstNext next">Next</button>
+          <div className="page">
+            <div className="title">Address</div>
+            <div className="field">
+              <div className="label">Line 1</div>
+              <input id="addLineOne" type="text" required />
+            </div>
+            <div className="field">
+              <div className="label">Line 2</div>
+              <input type="text" />
+            </div>
+            <div className="field btns">
+              <button className="prev-2 prev">Previous</button>
+              <button className="next-2 next">Next</button>
+            </div>
           </div>
-        </div>
-        <div className="page">
-          <div className="title">Contact Info:</div>
-          <div className="field">
-            <div className="label">Email Address</div>
-            <input id="emailAddress" type="email" required/>
+          <div className="page">
+            <div className="title">Date of Birth:</div>
+            <div className="field">
+              <div className="label">Date</div>
+              <input id="dob" type="date" required />
+            </div>
+            <div className="field">
+              <div className="label">Gender</div>
+              <select id="gender" required>
+                <option>Male</option>
+                <option>Female</option>
+                <option>Other</option>
+              </select>
+            </div>
+            <div className="field btns">
+              <button className="prev-3 prev">Previous</button>
+              <button className="next-3 next">Next</button>
+            </div>
           </div>
-          <div className="field">
-            <div className="label">Phone Number</div>
-            <input id="phoneNumber" type="Number" required/>
-          </div>
-          <div className="field btns">
-            <button className="prev-1 prev">Previous</button>
-            <button className="next-1 next">Next</button>
-          </div>
-        </div>
-        <div className="page">
-          <div className="title">Address</div>
-          <div className="field">
-            <div className="label">Line 1</div>
-            <input id="addLineOne" type="text" required/>
-          </div>
-          <div className="field">
-            <div className="label">Line 2</div>
-            <input type="text" />
-          </div>
-          <div className="field btns">
-            <button className="prev-2 prev">Previous</button>
-            <button className="next-2 next">Next</button>
-          </div>
-        </div>
-        <div className="page">
-          <div className="title">Date of Birth:</div>
-          <div className="field">
-            <div className="label">Date</div>
-            <input id="dob" type="date" required/>
-          </div>
-          <div className="field">
-            <div className="label">Gender</div>
-            <select id="gender" required>
-              <option>Male</option>
-              <option>Female</option>
-              <option>Other</option>
-            </select>
-          </div>
-          <div className="field btns">
-            <button className="prev-3 prev">Previous</button>
-            <button className="next-3 next">Next</button>
-          </div>
-        </div>
-        <div className="page">
-          <div className="title">Login Details:</div>
-          <div className="field">
-            <div className="label">Username</div>
-            <input id="username" type="text" required/>
-          </div>
-          <div className="field">
-            <div className="label">Password</div>
-            <input id="password" type="password" required/>
-          </div>
-          <div className="field">
-            <div className="label">Confirm Password</div>
-            <input id="confirmPassword" type="password" required/>
-          </div>
-          <div className="field btns">
+          <div className="page">
+            <div className="title">Login Details:</div>
+            <div className="field">
+              <div className="label">Username</div>
+              <input id="username" type="text" required />
+            </div>
+            <div className="field">
+              <div className="label">Password</div>
+              <input id="password" type="password" required />
+            </div>
+            <div className="field">
+              <div className="label">Confirm Password</div>
+              <input id="confirmPassword" type="password" required />
+            </div>
+            <div className="field btns">
             <button className="prev-4 prev">Previous</button>
-            <button className="submit">Submit</button>
+            <button className="submit" onClick={handleFormSubmit}>
+              Submit
+            </button>
           </div>
         </div>
       </form>
