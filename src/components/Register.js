46a3bb645a19288b4c0ad0ce4f23fc87af7f4f30
Harsha <khurdhulaharshavardhan@gmail.com>
2023-04-24 14:07:05 -0400

    allows user to perform basic registration.

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,176 +5,225 @@ import bcrypt from "bcryptjs";
 
 function Register(){
 
-const navigate = useNavigate();
-
-const [formData, setFormData] = useState({
-  firstName: "",
-  lastName: "",
-  emailAddress: "",
-  phoneNumber: "",
-  addLineOne: "",
-  dob: "",
-  gender: "Male",
-  username: "",
-  password: "",
-  confirmPassword: "",
-});
-
-//every change within the form data, must reflect in our state.
-const storeUserData = async () => {
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(userData.password, salt);
-  const dataToStore = { ...userData, password: hashedPassword };
-  localStorage.setItem("userData", JSON.stringify(dataToStore));
-};
-
-const checkEmptyFields = (page) => {
-    const requiredFields = {
-      1: ["firstName", "lastName"],
-      2: ["emailAddress", "phoneNumber"],
-      3: ["addLineOne"],
-      4: ["dob", "gender", "username", "password", "confirmPassword"],
-    };
-
-    for (const field of requiredFields[page]) {
-      if (formData[field] === "") {
-        alert(`Please fill the ${field} field.`);
-        return false;
+  const navigate = useNavigate();
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    emailAddress: "",
+    phoneNumber: "",
+    addLineOne: "",
+    addLineTwo: "",
+    dob: "",
+    gender: "",
+    username: "",
+    password: "",
+  });
+  
+  const iterateUserObjects = () => {
+    for (let key in localStorage) {
+      if (key.startsWith("user_")) {
+        const user = JSON.parse(localStorage.getItem(key));
+        console.log(`User Key: ${key}`);
+        console.log(`Username: ${user.username}`);
+        console.log(`Email: ${user.email}`);
+        console.log(`First Name: ${user.firstName}`);
+        console.log(`Last Name: ${user.lastName}`);
+        console.log(`Phone Number: ${user.phoneNumber}`);
+        console.log(`Address Line 1: ${user.addLineOne}`);
+        console.log(`Address Line 2: ${user.addLineTwo}`);
+        console.log(`Date of Birth: ${user.dob}`);
+        console.log(`Gender: ${user.gender}`);
+        console.log(`Hashed Password: ${user.hashedPassword}`);
+        console.log("-----------");
       }
     }
-    return true;
   };
+  iterateUserObjects();
 
+  const isUsernameTaken = (username) => {
+    const user = localStorage.getItem(username);
+    return user !== null;
+  };
+  
+  
+  iterateUserObjects();
 
+  useEffect(() => {
+    const slidePage = document.querySelector(".slide-page");
+    const nextBtnFirst = document.querySelector(".firstNext");
+    const prevBtnSec = document.querySelector(".prev-1");
+    const nextBtnSec = document.querySelector(".next-1");
+    const prevBtnThird = document.querySelector(".prev-2");
+    const nextBtnThird = document.querySelector(".next-2");
+    const prevBtnFourth = document.querySelector(".prev-3");
+    const nextBtnFourth = document.querySelector(".next-3");
+    const prevBtnFifth = document.querySelector(".prev-4");
+    const submitBtn = document.querySelector(".submit");
+    const progressText = document.querySelectorAll(".step p");
+    const progressCheck = document.querySelectorAll(".step .check");
+    const bullet = document.querySelectorAll(".step .bullet");
+    let current = 1;
 
-//lets validate the form data at the end of submission.
-const validateForm = () => {
-  for (const key in formData) {
-    if (formData[key] === "" && key !== "addLineTwo") {
-      alert(`Please fill the ${key} field.`);
-      return false;
-    }
-  }
-  if (formData.password !== formData.confirmPassword) {
-    alert("Passwords do not match.");
-    return false;
-  }
-  return true;
-};
+    const isAtLeast18YearsOld = (dob) => {
+      const birthDate = new Date(dob);
+      const currentDate = new Date();
+      const age = currentDate.getFullYear() - birthDate.getFullYear();
+      const monthDiff = currentDate.getMonth() - birthDate.getMonth();
+    
+      if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
+        return age - 1 >= 18;
+      }
+      return age >= 18;
+    };
+    
 
-//when user hits submit, based on the fact if user already exists we must then register them.
-const handleFormSubmit = async (e) => {
-  e.preventDefault();
-  if (validateForm()) {
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(formData.password, salt);
-    const storedData = { ...formData, password: hashedPassword };
-    localStorage.setItem("userData", JSON.stringify(storedData));
-    setTimeout(function () {
-      alert("Your Form Successfully Signed up");
-      navigate("/", { replace: true });
-    }, 800);
-  }
-};
+    const isValidStep = (step) => {
+      if (step === 1) {
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        if (!firstName || !lastName) {
+          alert("Please fill in both your first and last name.");
+          return false;
+        }
+      } else if (step === 2) {
+        const emailAddress = document.getElementById("emailAddress").value;
+        const phoneNumber = document.getElementById("phoneNumber").value;
+        if (!emailAddress || !phoneNumber) {
+          alert("Please fill in both your email address and phone number.");
+          return false;
+        }
+      } else if (step === 3) {
+        const addLineOne = document.getElementById("addLineOne").value;
+        if (!addLineOne) {
+          alert("Please fill in your address.");
+          return false;
+        }
+      } else if (step === 4) {
+        const dob = document.getElementById("dob").value;
+        if (!isAtLeast18YearsOld(dob)) {
+          alert("You must be at least 18 years old to register.");
+          return false;
+        }
+      } else if (step === 5) {
+        const username = document.getElementById("username").value;
+        if (isUsernameTaken(username)) {
+          alert("This username already exists. Please choose another one.");
+          return false;
+        }
+      }
+      return true;
+    };
 
-useEffect(() => {
-const slidePage = document.querySelector(".slide-page");
-const nextBtnFirst = document.querySelector(".firstNext");
-const prevBtnSec = document.querySelector(".prev-1");
-const nextBtnSec = document.querySelector(".next-1");
-const prevBtnThird = document.querySelector(".prev-2");
-const nextBtnThird = document.querySelector(".next-2");
-const prevBtnFourth = document.querySelector(".prev-3");
-const nextBtnFourth = document.querySelector(".next-3");
-const prevBtnFifth = document.querySelector(".prev-4");
-const submitBtn = document.querySelector(".submit");
-const progressText = document.querySelectorAll(".step p");
-const progressCheck = document.querySelectorAll(".step .check");
-const bullet = document.querySelectorAll(".step .bullet");
-let current = 1;
+    const storeUserData = async () => {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(userData.password, salt);
+      const dataToStore = { ...userData, password: hashedPassword };
+      localStorage.setItem("userData", JSON.stringify(dataToStore));
+    };
 
-nextBtnFirst.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
-nextBtnSec.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
+    nextBtnFirst.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (isValidStep(1)) {
+        slidePage.style.marginLeft = "-25%";
+        bullet[current - 1].classList.add("active");
+        progressCheck[current - 1].classList.add("active");
+        progressText[current - 1].classList.add("active");
+        current += 1;
+      }
+    });
 
-nextBtnThird.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-75%";
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-});
-nextBtnFourth.addEventListener("click", function(event){
-    event.preventDefault();
-    slidePage.style.marginLeft = "-100%";
-    bullet[current - 1].classList.add("active");
-    progressCheck[current - 1].classList.add("active");
-    progressText[current - 1].classList.add("active");
-    current += 1;
-  });
+    nextBtnSec.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (isValidStep(2)) {
+        slidePage.style.marginLeft = "-50%";
+        bullet[current - 1].classList.add("active");
+        progressCheck[current - 1].classList.add("active");
+        progressText[current - 1].classList.add("active");
+        current += 1;
+      }
+    });
 
-submitBtn.addEventListener("click", function(){
-  bullet[current - 1].classList.add("active");
-  progressCheck[current - 1].classList.add("active");
-  progressText[current - 1].classList.add("active");
-  current += 1;
-  setTimeout(function(){
-    alert("Your Form Successfully Signed up");
-    navigate('/', {replace: true});
-  },800);
-});
+    nextBtnThird.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (isValidStep(3)) {
+        slidePage.style.marginLeft = "-75%";
+        bullet[current - 1].classList.add("active");
+        progressCheck[current - 1].classList.add("active");
+        progressText[current - 1].classList.add("active");
+        current += 1;
+      }
+    });
 
-prevBtnSec.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "0%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
+    nextBtnFourth.addEventListener("click", function (event) {
+      event.preventDefault();
+      if (isValidStep(4)) {
+        slidePage.style.marginLeft = "-100%";
+        bullet[current - 1].classList.add("active");
+        progressCheck[current - 1].classList.add("active");
+        progressText[current - 1].classList.add("active");
+        current += 1;
+      }
+    });
 
-prevBtnThird.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-25%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
+    submitBtn.addEventListener("click", async function (event) {
+      event.preventDefault();
+      if (isValidStep(5)) {
+        bullet[current - 1].classList.add("active");
+        progressCheck[current - 1].classList.add("active");
+        progressText[current - 1].classList.add("active");
+        current += 1;
+        await storeUserData();
+        setTimeout(function () {
+          alert("Your Form Successfully Signed up");
+          navigate("/", { replace: true });
+        }, 800);
+      }
+    });
 
-prevBtnFourth.addEventListener("click", function(event){
-  event.preventDefault();
-  slidePage.style.marginLeft = "-50%";
-  bullet[current - 2].classList.remove("active");
-  progressCheck[current - 2].classList.remove("active");
-  progressText[current - 2].classList.remove("active");
-  current -= 1;
-});
+    prevBtnSec.addEventListener("click", function (event) {
+      event.preventDefault();
+      slidePage.style.marginLeft = "0%";
+      bullet[current - 2].classList.remove("active");
+      progressCheck[current - 2].classList.remove("active");
+      progressText[current - 2].classList.remove("active");
+      current -= 1;
+    });
 
-prevBtnFifth.addEventListener("click", function(event){
-    event.preventDefault();
-    slidePage.style.marginLeft = "-75%";
-    bullet[current - 2].classList.remove("active");
-    progressCheck[current - 2].classList.remove("active");
-    progressText[current - 2].classList.remove("active");
-    current -= 1;
-  });
-});
+    prevBtnThird.addEventListener("click", function (event) {
+      event.preventDefault();
+      slidePage.style.marginLeft = "-25%";
+      bullet[current - 2].classList.remove("active");
+      progressCheck[current - 2].classList.remove("active");
+      progressText[current - 2].classList.remove("active");
+      current -= 1;
+    });
 
+    prevBtnFourth.addEventListener("click", function (event) {
+      event.preventDefault();
+      slidePage.style.marginLeft = "-50%";
+      bullet[current - 2].classList.remove("active");
+      progressCheck[current - 2].classList.remove("active");
+      progressText[current - 2].classList.remove("active");
+      current -= 1;
+    });
+
+    prevBtnFifth.addEventListener("click", function (event) {
+      event.preventDefault();
+      slidePage.style.marginLeft = "-75%";
+      bullet[current - 2].classList.remove("active");
+      progressCheck[current - 2].classList.remove("active");
+      progressText[current - 2].classList.remove("active");
+      current -= 1;
+    });
+  }, []);
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setUserData((prevData) => ({
+      ...prevData,
+      [id]: value,
+    }));
+  };
 
 return(
 <>
@@ -299,7 +348,7 @@ return(
             </div>
             <div className="field btns">
             <button className="prev-4 prev">Previous</button>
-            <button className="submit" onClick={handleFormSubmit}>
+            <button className="submit" >
               Submit
             </button>
           </div>
